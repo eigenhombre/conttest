dce516401b2fa90e44935544be1ec26a72365135
John Jacobsen <john@mail.npxdesigns.com>
2012-12-23 18:41:50 -0600

    Bump version again

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='conttest',
-      version='0.0.2',
+      version='0.0.3',
       author='John Jacobsen',
       author_email='john@mail.npxdesigns.com',
       packages=['conttest'],
